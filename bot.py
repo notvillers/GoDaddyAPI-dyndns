af3bb04fb63bd5d13c3c39c8c1f23f15ea8ea3e7
Krisztián Villers <117743062+notvillers@users.noreply.github.com>
2023-09-13 20:53:41 +0200

    comments

--- bot.py
+++ bot.py
@@ -150,7 +150,9 @@ if api_available and daddy_available:
         userAccount = Account(api_key = d_key, api_secret = d_secret)
         userClient = Client(userAccount)
         try:
+            # Gets current GoDaddy IP
             currentIP = userClient.get_records(d_domain, record_type = d_type, name = d_record)
+            # Updates GoDaddy's IP to the stored one
             if (ip_stored != currentIP[0]["data"]):
                 updateResult = userClient.update_record_ip(d_ip_stored, d_domain, d_record, d_type)
             if updateResult is True:
