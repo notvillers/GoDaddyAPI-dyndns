6496a196950f9b2ad558a9c1be5dc969b1228a21
Krisztián Villers <117743062+notvillers@users.noreply.github.com>
2023-09-14 12:31:51 +0200

    replacing command with urlib and .md

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ If you do not have or do not want to access any DynDNS service, this solution ca
 > GoDaddy API is active, but it is under testing.
 
 ### login.txt
-On the first run, if you do not have the **login.txt** in the running/working directory, The script will generate an empty one. It is necessary for the email sending and should look like this:
+On the first run, if you do not have the **login.txt** in the running/working directory it will generate one with example data. It is necessary for the email sending and should look like this:
 
 ```
 sender@example_mail.com
@@ -30,7 +30,7 @@ where `{path_to_directory}` is the full path like `/home/user/bot/`
 _In this instance the crontab runs the script every 15 mins._
 
 ### GoDaddy DNS update with API
-For this, you need to create and configure a **daddy_api.txt** like this:
+On the first run, if you do not have the **daddy_api.txt** in the running/working directory it will generate one with example data. It is necessary for the GoDaddy API and should look like this:
 
 ```
 example_domain.com
@@ -39,6 +39,3 @@ example_name (@)
 api_key_example
 api_secret_example
 ```
-
-> [!IMPORTANT]
-> At the moment, the code is not going to fill the **daddy_api.txt** with example like it does with the **login.txt** and won't check if you modified it.
--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import subprocess
 import os
 import smtplib, ssl
@@ -5,9 +7,43 @@ from email.mime.text import MIMEText
 import sys
 import time
 from godaddypy import Client, Account
+import urllib.request
 
-# Command to send to the CLI
-command = "curl ipv4.icanhazip.com"
+# DEF: get IP
+def get_ip():
+    with urllib.request.urlopen(urllib.request.Request("https://ipv4.icanhazip.com")) as response:
+        return response.read().decode('utf-8')
+
+# DEF: send mail
+def send_email(subject, body, sender, recipients, password):
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = ', '.join(recipients)
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp, 465, context=context) as smtp_server:
+        smtp_server.login(sender, password)
+        smtp_server.sendmail(sender, recipients, msg.as_string())
+        print("Message sent!")
+
+# DEF: GoDaddy API
+def daddy_api(d_key, d_secret, d_domain, d_type, d_record, d_ip_stored):
+    userAccount = Account(api_key = d_key, api_secret = d_secret)
+    userClient = Client(userAccount)
+    global updateResult
+    try:
+        # Gets current GoDaddy IP
+        currentIP = userClient.get_records(d_domain, record_type = d_type, name = d_record)
+        # Updates GoDaddy's IP to the stored one
+        if (d_ip_stored != currentIP[0]["data"]):
+            updateResult = userClient.update_record_ip(d_ip_stored, d_domain, d_record, d_type)
+        if updateResult is True:
+            print('Updated DNS record to: ' + d_ip_stored)
+        else:
+            print('Checked the DNS record, no update needed.')
+    except:
+        print(sys.exc_info()[1])
+        sys.exit()
 
 # Location argument
 argument1 = ""
@@ -29,63 +65,55 @@ send_mail = True
 daddy_available = False
 api_available = False
 
-# If daddy_api.txt
+# If daddy_api.txt not found, then generating one
 if os.path.exists(daddy_api_file):
     api_available = True
     daddy_available = True
+else:
+    print(daddy_api_file + " not found, generating")
+    daddy_api_example = ["example_domain.com", "example_type (A)", "example_name (@)", "api_key_example", "api_secret_example"]
+    with open(daddy_api_file, 'w') as file:
+        for line in daddy_api_example:
+            file.write(line + "\n")
+    print(daddy_api_file + " created and filled with example data!")
 
 # If logint.txt is not found, then generating one
 if not os.path.exists(login_file) and send_mail:
-    print("login.txt not found, generating...")
+    print(login_file + " not found, generating...")
     login_example = ["sender@example_mail.com", "password", "receiver@example_mail.com", "smtp.example_mail.com"]
     with open(login_file, 'w') as file:
         for line in login_example:
             file.write(line + "\n")
-    print("login.txt created and filled with example data! \nExiting...")
+    print(login_file + " created and filled with example data!")
     send_mail = False
 
-# Check if ip.txt exists
+# If ip.txt is not found, then generating one, else stores it in ip_stored
 if os.path.exists(ip_file):
     file_ready = True
-    # Read ip.txt
     with open(ip_file, 'r') as file:
         ip_stored = file.read().lstrip().rstrip()
-        # Check if it has anything stored inside
         if ip_stored != "":
             ip_ready = True
-# If ip.txt not found then it creates it
 else:
     print(ip_file + " not found, generating...")
     with open(ip_file, 'w'):
         print(ip_file + " created")
 
-# Getting IP
 try:
-    # Run the command in the shell and capture the output
-    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    # Wait for the command to complete and capture the output
-    stdout, stderr = process.communicate()
-    # Check if the command was successful (return code 0)
-    if process.returncode == 0:
-        # Starting of the IP compare
-        ip_got = stdout.decode('utf-8').lstrip().rstrip()
-        # If we got IP stored in ip.txt (or anything stored in it)
-        if ip_ready:
-            # If the 2 IPs are equal it does nothing
-            if ip_stored != ip_got:
-                with open(ip_file, 'w') as file:
-                    file.write(ip_got)
-                ip_changed = True
-        # If file_name was empty or it was just created then writes the actual IP in it
-        else:
+    # Getting IP
+    ip_got = get_ip()
+    # Starting of the IP compare
+    if ip_ready:
+        # If the 2 IPs are equal it does nothing
+        if ip_stored != ip_got:
             with open(ip_file, 'w') as file:
                 file.write(ip_got)
             ip_changed = True
+    # If file_name was empty or it was just created then writes the actual IP in it
     else:
-        # Print any error messages
-        print("Error (cli):", stderr.decode('utf-8'))
-        time.sleep(5)
-        sys.exit()
+        with open(ip_file, 'w') as file:
+            file.write(ip_got)
+        ip_changed = True
 except Exception as e:
     print("Error (ip get):", str(e))
     time.sleep(5)
@@ -108,7 +136,7 @@ if ip_changed and send_mail:
 
     # login.txt configuration check
     if login[0] == "sender@example_mail.com" and not api_available:
-        print("login.txt is not configured properly.\nExiting...")
+        print("login.txt is not configured properly, GoDaddy API not available.\nExiting...")
         time.sleep(5)
         sys.exit()
 
@@ -116,18 +144,6 @@ if ip_changed and send_mail:
     process = subprocess.Popen("hostname", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
     hostname = stdout.decode('utf-8').lstrip().rstrip()
-
-    # DEF: send mail
-    def send_email(subject, body, sender, recipients, password):
-        msg = MIMEText(body)
-        msg['Subject'] = subject
-        msg['From'] = sender
-        msg['To'] = ', '.join(recipients)
-        context = ssl.create_default_context()
-        with smtplib.SMTP_SSL(smtp, 465, context=context) as smtp_server:
-            smtp_server.login(sender, password)
-            smtp_server.sendmail(sender, recipients, msg.as_string())
-            print("Message sent!")
 
     # Sending mail
     print("Calling send_email")
@@ -144,24 +160,6 @@ if api_available and daddy_available:
     # Reading api.txt
     with open(daddy_api_file, 'r') as file:
         api_data = [line.strip() for line in file.readlines()]
-    
-    # DEF: GoDaddy API
-    def daddy_api(d_key, d_secret, d_domain, d_type, d_record, d_ip_stored):
-        userAccount = Account(api_key = d_key, api_secret = d_secret)
-        userClient = Client(userAccount)
-        try:
-            # Gets current GoDaddy IP
-            currentIP = userClient.get_records(d_domain, record_type = d_type, name = d_record)
-            # Updates GoDaddy's IP to the stored one
-            if (ip_stored != currentIP[0]["data"]):
-                updateResult = userClient.update_record_ip(d_ip_stored, d_domain, d_record, d_type)
-            if updateResult is True:
-                print('Updated DNS record and wrote IP file.')
-            else:
-                print('Checked the DNS record, no update needed.')
-        except:
-            print(sys.exc_info()[1])
-            sys.exit()
 
     # Sending API call
     print("Calling daddy_api")
