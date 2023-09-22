55b89c1845f043e81b936e525f6a4150f0a471c7
Krisztián Villers <117743062+notvillers@users.noreply.github.com>
2023-09-22 12:21:13 +0200

    validator error

--- funct/json_read.py
+++ funct/json_read.py
@@ -5,21 +5,31 @@ import os
 
 # JSON validator
 def json_validator(json_str):
+
     try:
         json.loads(json_str)
+
         return True
+    
     except ValueError:
+
         return False
 
 # JSON read
 def daddy_api(file):
 
     if os.path.exists(file):
+
         with open(file, 'r') as json_file:
-            api_datas = json.load(json_file)
-        if json_validator(api_datas) is True:
+            json_data = json_file.read()
+
+        if json_validator(json_data) is True:
+
+            with open(file, 'r') as json_file:
+                api_datas = json.load(json_file)
             api_list = []
             i = 0
+
             for api_data in api_datas:
                     api_row = []
                     api_row.append(api_data["domain"])
@@ -29,8 +39,10 @@ def daddy_api(file):
                     api_row.append(api_data["api_secret"])
                     api_list.append(api_row)
                     i += 1
+
         else:
             i = -1
+
     else:
         example_data = [
             {
@@ -41,6 +53,7 @@ def daddy_api(file):
             "api_secret": "Your_secret_API_key"
             }
         ]
+        
         with open(file, 'w') as file:
             json.dump(example_data, file)
         i = 0
