ede0eabc7dde9cfc9067d256f5e3e77c8ec31ff9
Villers <117743062+notvillers@users.noreply.github.com>
2023-09-23 03:23:47 +0200

    beautify

--- funct/dns_email.py
+++ funct/dns_email.py
@@ -8,7 +8,9 @@ def send_email(subject, body, sender, recipients, password, smtp):
     msg['From'] = sender
     msg['To'] = ', '.join(recipients)
     context = ssl.create_default_context()
+    
     with smtplib.SMTP_SSL(smtp, 465, context=context) as smtp_server:
         smtp_server.login(sender, password)
         smtp_server.sendmail(sender, recipients, msg.as_string())
+        
         return "E-mail sent! (from: " + sender + " to " + ', '.join(recipients) + ")"
--- funct/log_insert.py
+++ funct/log_insert.py
@@ -7,8 +7,11 @@ import os
 def insert(log_file, summary_cli):
     print(summary_cli)
     current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+   
     if not os.path.exists(log_file):
+       
         with open(log_file, 'w') as file: 
             file.write(current_time + ": " + log_file + " created \n")
+   
     with open(log_file, 'a') as file: 
         file.write(current_time + ": " + summary_cli + "\n")
